cad118d141af1a5bce0a994ad527074a3b92b158
Belim <github@mirinsoft.com>
2020-01-21 17:24:11 +0100

    Create wim_tweak_switches.cmd

---
+++ third-party/wim_tweak_switches.cmd
@@ -0,0 +1,34 @@
+@Credits goes to @https://gist.github.com/Espionage724/35a6776120b27fb0f892955ce645a763
+
+install_wim_tweak.exe /o /c "Microsoft-Windows-Internet" /r
+install_wim_tweak.exe /o /c "Adobe-Flash" /r
+install_wim_tweak.exe /o /c "Microsoft-Windows-Cortana" /r
+install_wim_tweak.exe /o /c "Microsoft-Windows-Search2" /r
+install_wim_tweak.exe /o /c "Microsoft-Windows-SearchEngine" /r
+install_wim_tweak.exe /o /c "Microsoft-Windows-RetailDemo" /r
+install_wim_tweak.exe /o /c "Microsoft-Windows-ContactSupport" /r
+install_wim_tweak.exe /o /c "Microsoft-PPIProjection" /r
+install_wim_tweak.exe /o /c "Microsoft-Windows-Backup" /r
+install_wim_tweak.exe /o /c "Windows-Defender" /r
+
+install_wim_tweak.exe /o /c "Microsoft-Xbox" /r
+install_wim_tweak.exe /o /c "Microsoft-Windows-TroubleShooting" /r
+install_wim_tweak.exe /o /c "Microsoft-OneCore-TroubleShooting" /r
+install_wim_tweak.exe /o /c "Microsoft-Windows-LanguageFeatures" /r
+install_wim_tweak.exe /o /c "Microsoft-Windows-Legacy" /r
+install_wim_tweak.exe /o /c "Microsoft-OneCore-SpeechComponents" /r
+install_wim_tweak.exe /o /c "Microsoft-OneCore-Gaming" /r
+install_wim_tweak.exe /o /c "Microsoft-OneCore-Indexer" /r
+install_wim_tweak.exe /o /c "Microsoft-Windows-BioEnrollment" /r
+install_wim_tweak.exe /o /c "Microsoft-OneCore-Biometrics" /r
+
+install_wim_tweak.exe /o /c "Microsoft-Windows-Printing-XPSServices" /r
+install_wim_tweak.exe /o /c "Microsoft-Windows-Xps" /r
+install_wim_tweak.exe /o /c "Microsoft-Windows-Geolocation" /r
+install_wim_tweak.exe /o /c "Microsoft-Windows-Compression" /r
+install_wim_tweak.exe /o /c "Microsoft-Windows-BusinessScanning" /r
+install_wim_tweak.exe /o /c "Microsoft-Windows-MediaPlayer" /r
+install_wim_tweak.exe /o /c "Microsoft-Windows-WindowsMediaPlayer" /r
+install_wim_tweak.exe /o /c "Microsoft-Windows-VirtualPC" /r
+install_wim_tweak.exe /o /c "Microsoft-Windows-VirtualXP" /r
+install_wim_tweak.exe /o /c "Microsoft-Windows-SystemRestore" /r
